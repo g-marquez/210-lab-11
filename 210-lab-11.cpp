8c544ea0d34858fc79b278c2edd92b1eb7733627
Gabe Marquez <gabrielmarquez98@gmail.com>
2026-02-21 18:01:10 -0800

    Initial commit: starting lab 11

---
+++ 210-lab-11.cpp
@@ -0,0 +1,10 @@
+//*****************************************************************************
+// COMSC-210 | Lab 11 | Gabriel Marquez
+// Description: this program features a simple dynamic array of structs used
+// to track NFL fans, their favorite teams, and their team's Super Bowl wins.
+//*****************************************************************************
+
+#include <iostream>
+#include <string>
+#include <fstream>
+using namespace std;
